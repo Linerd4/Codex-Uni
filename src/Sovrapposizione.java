105a4a6cde9d9429a31974bdd09d291bb1158aea
gfreni <167890150+gfreni@users.noreply.github.com>
2024-05-13 08:54:05 +0200

    Add files via upload
    Prima implementazione delle classi: Sovrapposizione e Visibilità

---
+++ src/Sovrapposizione.java
@@ -0,0 +1,45 @@
+package codex;
+import java.util.*;
+
+public class Sovrapposizione {
+private Carta altoSx;
+private Carta altoDx;
+private Carta bassoSx;
+private Carta bassoDx;
+
+public Sovrapposizione() {
+	this.altoDx=altoDx;
+	this.altoSx=altoSx;
+	this.bassoDx=bassoDx;
+	this.bassoSx=bassoSx;
+}
+
+public Carta getAltoSx() {
+	return altoSx;
+}
+
+public Carta getAltoDx() {
+	return altoDx;
+}
+
+public Carta getBassoSx() {
+	return bassoSx;
+}
+
+public Carta getBassoDx() {
+	return bassoDx;
+	
+}
+
+
+
+
+
+
+
+
+
+
+
+
+}
---
+++ src/Visibilità.java
@@ -0,0 +1,34 @@
+package codex;
+import java.util.*;
+
+public class Visibilità {
+private int altoSx;
+private int altoDx;
+private int bassoSx;
+private int bassoDx;
+
+public Visibilità(int altoSx,int altoDx,int bassoSx,int bassoDx) {
+	this.altoDx=altoDx;
+	this.altoSx=altoSx;
+	this.bassoDx=bassoDx;
+	this.bassoSx=bassoSx;
+
+}
+
+public int getAltoSx() {
+	return altoSx;
+}
+
+public int getAltoDx() {
+	return altoDx;
+}
+
+public int getBassoSx() {
+	return bassoSx;
+}
+
+public int getBassoDx() {
+	return bassoDx;
+}
+
+}
